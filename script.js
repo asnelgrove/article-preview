ee7604380e777abbd72fecbca3467a5237d4b516
Andrew Snelgrove <asnel@my.yorku.ca>
2023-05-08 19:20:19 -0230

    share button functional

---
+++ script.js
@@ -0,0 +1,6 @@
+const shareBtn = document.getElementsByClassName('share');
+const overlay = document.getElementById('overlay');
+
+function toggleHidden(overlay) {
+    overlay.classList.toggle('hidden')
+}
